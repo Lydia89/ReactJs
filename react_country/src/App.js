b875be7e009fd9b37264054f97f7c33b7666c25d
Lydia89 <lydy@live.fr>
2020-10-02 12:03:21 +0200

    exercice

--- react_country/src/App.js
+++ react_country/src/App.js
@@ -1,38 +1,68 @@
+
+
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Button from'./components/Button'
+import Button from './components/Button'
+
 
 class App extends Component {
-  /*
-  constructor(props){
-  
+
+  constructor() {
     super()
-    //this.componentDidMount=this.componentDidMount.bind(this)
-  }*/
+    this.Click = this.Click.bind(this)
+  }
+
   state = {
     name: '',
     capital: '',
     flag: '',
     population: 0,
     region: '',
+    active: ''
 
   }
   componentDidMount() {
 
+
     fetch('https://restcountries.eu/rest/v2/name/france')
       .then(res => res.json())
       .then(Data => {
-        console.log(Data)
+        // console.log(Data)
 
-        this.setState({ name : Data[0].name }); 
-        this.setState({ capital : Data[0].capital }); 
-        this.setState({ flag: Data[0].flag }); 
-        this.setState({ population: Data[0].population}); 
-        this.setState({ region: Data[0].region}); 
-        console.log(this.state.capital)
+        this.setState({ name: Data[0].name });
+        this.setState({ capital: Data[0].capital });
+        this.setState({ flag: Data[0].flag });
+        this.setState({ population: Data[0].population });
+        this.setState({ region: Data[0].region });
+        // console.log(this.state.capital)
       })
+      
+
   }
+  componentDidUpdate() {
+
+  fetch('https://restcountries.eu/rest/v2/name/brazil')
+      .then(res => res.json())
+      .then(Data => {
+        // console.log(Data)
+
+        this.setState({ name: Data[0].name });
+        this.setState({ capital: Data[0].capital });
+        this.setState({ flag: Data[0].flag });
+        this.setState({ population: Data[0].population });
+        this.setState({ region: Data[0].region });
+        // console.log(this.state.capital)
+      })
+    }
+
+  Click() {
+    this.setState({
+
+    })
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div className="App">
@@ -41,10 +71,19 @@ class App extends Component {
         <p>flag :{this.state.flag}</p>
         <p> population : {this.state.population}</p>
         <p>region : {this.state.region}</p>
-        <Button/>
+
+        <Button isSelected={this.state.active === "france"} onClick={this.Click}
+          name={this.state.name}
+          capital={this.state.capital}
+          flag={this.state.flag}
+          population={this.state.population}
+          region={this.state.region} >
+          France </Button>
+        <Button isSelected={this.state.active === "brazil"} onClick={this.Click}> Brazil</Button>
+        <Button isSelected={this.state.active === "croatia"} onClick={this.Click}> Croatia</Button>
 
       </div>
-      
+
     );
   }
 }
--- react_country/src/components/Button.js
+++ react_country/src/components/Button.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+
 class Button extends Component {
+   
+/*
 
-render(){
-    return(
-        <div>
+    constructor() {
 
-<button>France</button>
-
-        </div>
-    )
-}
+        super()
+        this.handleClick = this.handleClick.bind(this)
+      }
+    
+    handleClick() {
+    this.setState({
+        text:'send'
+    })
+      console.log(this.state)  
+      }
+    */
+   /*<button 
+             onClick={this.props.onClick} >
+            </button>*/
+    render() {
+        return (
+           
+                <div>
+                <button  className= {this.props.isSelected.toString() ? '  btn btn-secondary': ' btn btn-light '  }
+                 onClick={this.props.onClick} >
+                        
+                       {this.props.children}
+                     
+                    
+                </button>
+                
+             </div>
+        )
+    }
 
 
 } export default Button
