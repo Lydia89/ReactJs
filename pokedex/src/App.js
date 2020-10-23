ab648e6d27468bd6926f7ff4c84ab3e60d0e0f04
Lydia89 <lydy@live.fr>
2020-10-23 16:02:30 +0200

    pokedex

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,101 +1,78 @@
 import React, { Component } from 'react';
-
 import './App.css';
-import Card from './Card'
-import Button from './Button'
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.click = this.click.bind(this)
+    this.state = {
+      list: [],
+      currentPokemon: null
+    }
   }
-
-  state = {
-    id: '',
-    name: '',
-    height: 0,
-    type: [],
-    weight: 0,
-    img: ''
-  }
-
 
   componentDidMount() {
-    this.click("1")
-    /*
-        fetch('https://pokeapi.co/api/v2/pokemon/1')
-    
-          .then(res => res.json())
-          .then(json => {
-            console.log(json)
-            this.setState({
-              name: json.name,
-              height: json.height,
-              weight: json.weight,
-              type: json.types[0].type.name,
-            })
-            //console.log(json.name)
-           // console.log(json.height)
-           // console.log(json.weight)
-           // console.log(json.types[0].type.name)
-          })
-    */
-  }
-
-  click(val) {
-    //console.log("je click")
-    this.setState({
-      id: val
-
-
-    })
-    console.log(this.state.id)
-
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${val}`)
-
+    // Nous récupérons la liste de 100 pokemon au démarage de l'application
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
       .then(res => res.json())
       .then(json => {
         console.log(json)
         this.setState({
-          name: json.name,
-          height: json.height,
-          weight: json.weight,
-          type: json.types[0].type.name,
+          list: json.results
         })
-
       })
-
-
   }
 
+  click(url) {
+    // url corresponds aux informations que nous devons récupérer sur le pokemon selectionner
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
 
+       // let typespo = json.types.map(function (elem) {
+         // return elem.type.name
+       // }).join(',')
+      //  console.log('typespo',typespo)
+        // nous stokons dans currentPokemon les informations du pokemon selectionner
+        this.setState({
+          currentPokemon: json
+        })
+      })
+  }
   render() {
-
     return (
-      <div className="App">
+      <div className="App" >
+        <section className="single-poke jumbotron" style={{ backgroundColor: "black", color: "white" }}  >
+          {this.state.currentPokemon ?
+            (<div className="poke-info">
+              <img className="big" src={`https://pokeres.bastionbot.org/images/pokemon/${this.state.currentPokemon.id}.png`} style={{ width: 150 }}/>
+              <div>
+              
+                <p><b>Name :</b> {this.state.currentPokemon.name}</p>
+                <p><b>Height :</b>{this.state.currentPokemon.height}</p>
+                <p><b>Weight :</b>{this.state.currentPokemon.weight}</p>
+                <p ><b>Type :</b>{this.state.currentPokemon.types.map(function (elem){
+                return elem.type.name   
+                  }) .join(',')} </p>
+        
 
-        <Card
-          name={this.state.name}
-          height={this.state.height}
-          weight={this.state.weight}
-          type={this.state.type}
-
-          onClick={() => this.click("1")}
-
-        ></Card >
-        <Button
-          name={this.state.name}
-          height={this.state.height}
-          weight={this.state.weight}
-          type={this.state.type}
-          onClick={() => this.click("2")}
-
-        ></Button>
-
-
-
-      </div >
+              </div>
+            </div>) :
+            (<h1>Please select a pokemon</h1>)
+          }
+        </section>
+        <section>
+          {this.state.list.map((elem, index) => {
+            return (
+              <button onClick={() => this.click(elem.url)}>
+                <img src={`https://pokeres.bastionbot.org/images/pokemon/${index + 1}.png`} style={{ width: 150 }}/>
+                <p>{elem.name}</p>
+              </button>
+            )
+          })}
+        </section>
+      </div>
     );
   }
 }
